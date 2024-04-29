6f5cbe776e9285a042467b1fe523c3a130a89a49
sameer khan <sameerkhanaa5@gmail.com>
2024-04-29 13:09:00 +0530

    create bin form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CreateBinComponent } from './components/create-bin/create-bin.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'create', component: CreateBinComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
