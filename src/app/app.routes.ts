5411ba22f3d6963f852644d80c4c2ffb76fcb092
sameerkindia <sameerkhanaa5@gmail.com>
2024-05-25 06:40:07 +0530

    auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { CreateBinComponent } from './components/create-bin/create-bin.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'create', component: CreateBinComponent },
+  { path: 'create', component: CreateBinComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
